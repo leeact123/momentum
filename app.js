b263f292134a71b471508226b55e74d67240352f
leecode <lucky332533@gmail.com>
2022-11-04 19:09:13 +0900

    edit login-form

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const greeting = document.querySelector("#greeting");
 function onLoginBtnClick(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username); 
-    paintGreeting(username);
+    localStorage.setItem(USERNAME_KEY, loginInput.value); 
+    paintGreeting();
 }
 
-function paintGreeting(username) {
+function paintGreeting() {
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -25,5 +25,5 @@ if(savedUsername === null){
      loginForm.classList.remove(HIDDEN_CLASSNAME);
      loginForm.addEventListener("submit", onLoginBtnClick);
 } else {
-    paintGreeting(savedUsername);
+    paintGreeting();
 }

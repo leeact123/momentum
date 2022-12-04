1b85a47adc686e901d5f4f50ec2351da8d2d0b64
leecode <lucky332533@gmail.com>
2022-12-04 18:11:17 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -2,6 +2,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const mainScreen = document.querySelector(".main-screen");
 
  const HIDDEN_CLASSNAME = "hidden";
  const USERNAME_KEY = "username";
@@ -15,8 +16,10 @@ function onLoginBtnClick(event) {
 
 function paintGreeting() {
     const username = localStorage.getItem(USERNAME_KEY);
-    greeting.innerText = `Hello ${username}`;
+    greeting.innerText = `Hello! ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    mainScreen.classList.remove(HIDDEN_CLASSNAME);
+
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
